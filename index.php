3e63516ff3aa1389e1810305181a1fb1de5ac738
Karl Morisset <karl.morisset.register@gmail.com>
2021-10-05 00:31:41 +0200

    resultat attendu

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css"></link>
 </head>
 <body>
-    
+    <?php include "header.php"; ?>
+
+    <main>
+        <h2>Page principale</h2>
+    </main>
 </body>
 </html>
